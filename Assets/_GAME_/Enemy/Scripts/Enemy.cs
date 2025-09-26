8d67caeb97b1122200051162185a7f2e69bff8e9
DoThang-254 <thangmoneo2542004@gmail.com>
2025-09-26 22:37:50 +0700

    add enemy spawn

---
+++ Assets/_GAME_/Enemy/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public Transform player;   // gán trong Inspector hoặc tự tìm
+    public float speed = 2f;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        // hướng từ enemy đến player
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        // di chuyển enemy về phía player
+        rigid.linearVelocity = direction * speed;
+    }
+}
---
+++ Assets/_GAME_/Enemy/Scripts/EnemySpawned.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawned : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> prefabs;
+    [SerializeField] List<float> spawnRates; 
+    [SerializeField]
+    float period = 1;
+    [SerializeField]
+    float time = 0;
+    [SerializeField]
+    int poolSize = 10;
+    List<GameObject> pool;
+    void Start()
+    {
+        pool = new List<GameObject>();
+
+        for (int i = 0; i < prefabs.Count; i++)
+        {
+            int count = Mathf.RoundToInt(poolSize * spawnRates[i]);
+            for (int j = 0; j < count; j++)
+            {
+                GameObject o = Instantiate(prefabs[i]);
+                o.SetActive(false);
+                pool.Add(o);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= period)
+        {
+            GameObject enemy = GetGame();
+            if (enemy == null)
+            {
+
+            }
+            else
+            {
+                enemy.SetActive(true);
+            }
+
+            time = 0;
+
+        }
+    }
+
+    GameObject GetGame()
+    {
+        foreach (GameObject o in pool)
+
+            if (o.activeSelf == false) return o;
+        return null;
+    }
+}
